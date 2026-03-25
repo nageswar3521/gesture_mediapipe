c228c82f8f3b0e5057b9d9a6ebd69197752d7045
nageswar3521 <nageswarfet@kiit.ac.in>
2026-03-25 22:11:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ if (cmd === lastCommand) return;
 
 ```
 try {
-  await fetch(`${ESP32_IP}/${cmd}`, { mode: "no-cors" });
+  await fetch(`${ESP32_IP}/${cmd}`);
   lastCommand = cmd;
 
   statusBox.innerText = "🟢 CONNECTED";
